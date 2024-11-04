1e95f3f35113b3fa8bd4a12ae951cad931b1ee77
dohaelsawy <dohaelsawy18@gmail.com>
2024-11-04 10:46:42 +0200

    feat: add search ui

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -2,12 +2,15 @@
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/authStore';
+import { useAccountStore } from '@/stores/accountStore';
+
 import { ref } from 'vue';
 
 
 
 const router = useRouter();
 const authStore = useAuthStore();
+const accountStore = useAccountStore();
 const isLogin = ref(authStore.returnIsLogin);
 
 const onSubmitLogout = async () => {
@@ -36,6 +39,27 @@ const onSubmitLogin = async () => {
     router.push('/auth/login');
 };
 
+const searchTerm = ref("")
+const searchResult = ref("")
+const setSearchTerm = (event: Event) => {
+    const input = event.target as HTMLInputElement
+    searchTerm.value = input.value
+    console.log(searchTerm.value)
+}
+
+const sendSearchTerm = async () => {
+    if (searchTerm.value !== "") {
+        try {
+            searchResult.value = await accountStore.getUsernameSearch(searchTerm.value);
+            router.push(`/link_tree/${searchResult.value}`).then(() => {
+            window.location.reload();
+        });
+        } catch (error) {
+            router.push("/error")
+        }
+    }
+}
+
 </script>
 
 <template>
@@ -51,13 +75,19 @@ const onSubmitLogin = async () => {
                 <li class="nav-item">
                     <a @click="onSubmitLinkTree" class="nav-link" href="">Link Tree</a>
                 </li>
-                <li v-if="!isLogin" class="nav-item">
-                    <Button variant="secondary" @click="onSubmitLogin" cy="login-btn">Login</Button>
-                </li>
-                <li v-if="isLogin" class="nav-item">
+                <li v-show="isLogin" class="nav-item">
                     <a class="nav-link" :href="profileUrl" cy="profile">Profile</a>
                 </li>
-                <li v-if="isLogin" class="nav-item">
+                <li class="nav-item">
+                    <input type="search" name="search" id="search" placeholder="search username"
+                        class="bg-transparent  text-white outline-white px-3 py-3 border-white placeholder:text-white rounded-lg border-2"
+                        :onchange="setSearchTerm">
+                    <i class="fa-solid fa-magnifying-glass text-white px-3" @click="sendSearchTerm"></i>
+                </li>
+                <li v-show="!isLogin" class="nav-item">
+                    <Button variant="secondary" @click="onSubmitLogin" cy="login-btn">Login</Button>
+                </li>
+                <li v-show="isLogin" class="nav-item">
                     <Button variant="secondary" @click="onSubmitLogout" cy="logout-btn">Logout</Button>
                 </li>
             </ul>
